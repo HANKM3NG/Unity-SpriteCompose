e9407a63f9fb807f1cec4b7ea6ed4debfefeacbb
Hank <18844011@qq.com>
2019-04-10 00:25:30 +0800

    多张图片组合成一张

--- Assets/Scripts/SpriteCompose.cs
+++ Assets/Scripts/SpriteCompose.cs
@@ -1,41 +1,51 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteCompose : MonoBehaviour {
-    public Sprite Base0, Alpha0, Base1, Alpha1;
-    public SpriteData[] Sprites = new SpriteData[9];
     public SpriteRenderer Result;
-    private Texture2D tempTex2D;
+    public List<SpriteData> spriteData;
+
+    private Texture2D tempTexture2D;
+    private Color tempColor;
+    private Vector2Int usedPos;
 
     public void Compose () {
-        tempTex2D = new Texture2D (16, 16);
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 16; j++) {
-                Color gColor0 = Base0.texture.GetPixel ((int) Base0.textureRect.x + i, (int) Base0.textureRect.y + j);
-                Color gColor1 = Alpha1.texture.GetPixel ((int) Alpha1.textureRect.x + i, (int) Alpha1.textureRect.y + j);
-                if (gColor1.a == 0) {
-                    tempTex2D.SetPixel (i, j, gColor0);
-                } else {
-                    tempTex2D.SetPixel (i, j, gColor1);
+        tempTexture2D = new Texture2D (16, 16);
+        spriteData.Sort ((a, b) => -a.level.CompareTo (b.level));
+        usedPos = new Vector2Int (-1, -1);
+        int x, y;
+        if (spriteData.Count > 0) {
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < 16; j++) {
+                    for (int k = 0; k < spriteData.Count; k++) {
+                        x = (int) spriteData[k].sprite.textureRect.x;
+                        y = (int) spriteData[k].sprite.textureRect.y;
+                        tempColor = spriteData[k].sprite.texture.GetPixel (x + i, y + j);
+
+                        if (tempColor.a != 0 && usedPos != new Vector2Int (i, j)) {
+                            tempTexture2D.SetPixel (i, j, tempColor);
+                            usedPos = new Vector2Int (i, j);
+                        } else {
+                            continue;
+                        }
+                    }
                 }
             }
         }
-       
-        tempTex2D.filterMode = FilterMode.Point;
-        tempTex2D.Apply ();
 
-        Vector3 rPos = Result.transform.position;
-        Rect texRect = new Rect (0, 0, tempTex2D.width, tempTex2D.height);
-        Debug.LogFormat ("xmin:{0}, ymin:{1}, xmax:{2}, ymax:{3}", texRect.xMin, texRect.yMin, texRect.xMax, texRect.yMax);
-        Sprite sp = Sprite.Create (tempTex2D, texRect, texRect.center);
+        tempTexture2D.filterMode = FilterMode.Point;
+        tempTexture2D.Apply ();
+
+        Rect texRect = new Rect (0, 0, tempTexture2D.width, tempTexture2D.height);
+        Sprite sp = Sprite.Create (tempTexture2D, texRect, Vector2.zero);
         Result.sprite = sp;
     }
 
 }
 
+[Serializable]
 public class SpriteData {
-    public Sprite Base;
-    public Sprite Edge;
-    public int weight;
+    public Sprite sprite;
+    public int level;
 }
